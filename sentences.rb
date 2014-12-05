8020d9cd21306e0db8b1a34d29e8d051c37838d0
Apprentice <apprentice@dbc50.local>
2014-12-05 14:50:31 -0800

    Add sentences

---
+++ sentences.rb
@@ -0,0 +1,15 @@
+"__name__ held the __adj__ __object__ open while he __verb__."
+
+"__name__ __verb__ over the side of the __adj__ __object__."
+
+"The __adj__ __object__ was destroyed when __name__ __verb__."
+
+"Hello, __name__! Are you __verb__ the __adj__ __object__ today?"
+
+"On the __adj__ day of school, __name__ __verb__ to the wrong __object__."
+
+"__name__ was very __adj__ when she __verb__ the __object__."
+
+"One __adj__ day, __name__ was __verb__ along, when suddenly he tripped over a __object__."
+
+"A __object__ was __verb__ on the ground, when __name__ came across it, and was very __adj__."
